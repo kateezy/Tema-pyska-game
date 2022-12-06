6cf79902f3f48458a9280b8de7ac8405f5ddb87d
Kateezy <katyakorzihina8@gmail>
2022-12-07 00:24:36 +0300

    rect object
    changed inclination and position

--- tema.py
+++ tema.py
@@ -6,10 +6,12 @@ class Tema():
         """"инициализация к пушке"""
         self.screen = screen
         self.image = pygame.image.load('pyska/pixil-frame-0.png')
+        self.image = pygame.transform.rotate(self.image, -90)
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.x = 10
+        self.rect.y = 300
+        # self.rect.bottom = self.screen_rect.bottom
 
     def output(self):
         """рисование пушки"""
